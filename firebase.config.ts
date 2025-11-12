c031b93eacc94a8dd03af0e5ee42998bbc84ff1b
Oloche Mathias Echioda <mechioda@gmail.com>
2025-11-12 13:55:01 +0100

    Removed secrets from config file

--- firebase.config.ts
+++ firebase.config.ts
@@ -25,21 +25,3 @@ const db = getFirestore(app);
 
 export { app, auth, db };
 
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from 'firebase/auth';
-// import { getFirestore } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBh_fbq-B534yDlXHBk12cGW2pDY0RrRqI",
-//   authDomain: "framez-8a332.firebaseapp.com",
-//   projectId: "framez-8a332",
-//   storageBucket: "framez-8a332.firebasestorage.app",
-//   messagingSenderId: "589074868109",
-//   appId: "1:589074868109:web:1119ac9edc7f7da1c37fc4"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// const db = getFirestore(app);
-
-// export { app, auth, db };
